b5aa0c440792b6ae1fd756ec35f7fa4fb64019a3
grimharald <grimharald@mailfence.com>
2023-04-27 16:34:18 +0000

    minor change

--- games/axe/axe_game.cpp
+++ games/axe/axe_game.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
-    InitWindow(200,300,"huw");
+    float rootBeer; rootBeer = 1.99;
+    double cheeseBurger; cheeseBurger = 5.99;
+    bool shouldHaveLunch; shouldHaveLunch = true;
+    
+    int width; width = 200;
+    int height; height = 300;
+    InitWindow(width,height,"Huw");
 }
